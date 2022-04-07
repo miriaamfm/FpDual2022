194eba6c772d38649d5fb20a76e326cd9ec105c1
miriaamfm <miriaamfm@gmail.com>
2022-04-07 10:01:02 +0200

    Commit 1

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,13 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+	new Main().printMessage();
+
+    }
+
+    public void printMessage(){
+    System.out.println("hola");
+    }
+}
